094c82aca1125f932019ab5ce984b671ea2c4bd7
Patrick <rosevalley@gmail.com>
2023-05-26 17:06:19 +0200

    Initial Commit

---
+++ Restaurant splitting app/Restaurant_splitting_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restaurant_splitting_appApp.swift
+//  Restaurant splitting app
+//
+//  Created by User on 26/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Restaurant_splitting_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
