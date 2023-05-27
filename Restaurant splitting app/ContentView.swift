b1eb978228bfacad8939811daf8556ae7a7348d4
Patrick <rosevalley@gmail.com>
2023-05-27 19:56:30 +0200

    Did some refactoring, added function to get currency in correct format (GEL)

--- Restaurant splitting app/ContentView.swift
+++ Restaurant splitting app/ContentView.swift
@@ -8,19 +8,104 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+    @State private var checkAmount = 0.0
+    //add focus and add button somehow on the screen, to remove the focus. to remove the numerical keyboard when not needed.
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    //swiftUI's @State property wrapper lets us modify our view structs freely, which means as our program changes we can update our view properties to match.
+    
+    
+    let tipPercentages = Array(0..<101)
+                               
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
     }
+    
+    var totalAllPersons: Double {
+        _ = Double(numberOfPeople + 2) //_ was let peopleCountAll
+        let tipSelectionAll = Double(tipPercentage)
+        
+        let tipValueAll = checkAmount / 100 * tipSelectionAll
+        let grandTotalAll = checkAmount + tipValueAll
+        
+        return grandTotalAll
+    }
+    
+    var body: some View {
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: currencyFormat())
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0)people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                } header: {
+                    Text("Select a percentage to leave a tip")
+                }
+                
+                Section {
+                    Text(totalAllPersons, format: currencyFormat())
+                } header: {
+                    Text("Total amount with tip")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: currencyFormat())
+                } header: {
+                    Text("Amount per person")
+                }
+            }
+            .navigationTitle("weSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+
+let locale = Locale(identifier: "ka_GE")
+
+func currencyFormat() -> FloatingPointFormatStyle<Double>.Currency {
+    return .currency(code: locale.currency?.identifier ?? "GEL")
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//And this is something that they would never ever use in georgia
+///But still funny
